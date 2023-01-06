ba8e9b59aec5e53b980e4ceacf9bf8070e345910
Pranav132 <mais.06pranav@gmail.com>
2023-01-06 16:48:11 +0530

    mkm

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,18 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+function validateFormOnSubmit(theForm) {
+    var reason = "";
+    reason += validateName(theForm.name);
+    reason += validatePhone(theForm.phone);
+    reason += validateEmail(theForm.emaile);
+
+    if (reason != "") {
+        alert("Some fields need correction:\n" + reason);
+    } else {
+        simpleCart.checkout();
+    }
+    return false;
+}
